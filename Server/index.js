06c9aaf52412b6256c9488b844ff88c6f77c866c
YashVerma24299 <ritikverma23@gmail.com>
2024-11-18 14:31:32 +0530

    creating form and database connectivity

--- Server/index.js
+++ Server/index.js
@@ -1,41 +1,52 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const app =express();
-
-const PORT=8080;
-
-
-//to make the server response any incoming requests
-app.listen(PORT, () =>{
-    console.log("server started at port " + PORT);
+const nameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 });
+const nameModel = mongoose.model("name", nameSchema);
+mongoose.connect("mongodb://127.0.0.1:27017/");
 
-//array of objects
-const names=[
-    {
-        id:1,
-        name:"Ramesh",
+const app = express();
+const PORT = 3000;
 
-    },
-    {
-        id:2,
-        name:"Suresh",
-    },
+const names = [
+  {
+    id: 1,
+    name: "Ramesh",
+  },
+  {
+    id: 2,
+    name: "Suresh",
+  },
 ];
 
-app.get("/names",(req,res)=>{
-    res.send(names);
+app.use(cors({ origin: "http://localhost:5173" }));
+
+// TELLING MY SERVER TO ALLOW REQUESTS WITH DATA
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/names", (req, res) => {
+  res.send(names);
+});
+app.post("/add", (req, res) => {
+  console.log(req.body);
+  const newName = req.body;
+  const dataToAdd = new nameModel(newName);
+  dataToAdd.save();
+  res.send("Data Saved");
+});
+app.delete("/delete/:id", (req, res) => {
+  const idToDelete = req.params.id;
+  nameModel.findByIdAndDelete({ idToDelete });
+  res.send(names);
 });
 
-app.post("/add",(req,res)=>{
-    const newName= req.body();
-    const dataToAdd=new names(newName);
-    dataToAdd.save();
-    res.send(names);
-});
-
-app.delete("/delete/:id",(req,res)=>{
-    const idToDelete=req.params.id;
-    names.findByIdAndDelete({idToDelete});
-    res.send(names);
+app.listen(PORT, () => {
+  console.log("Server started at port " + PORT);
 });
