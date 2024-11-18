1d566a6342efb708fc429cb9e0cc8cdeda9218b4
YashVerma24299 <ritikverma23@gmail.com>
2024-11-18 11:28:33 +0530

    frontendsetup

--- Server/index.js
+++ Server/index.js
@@ -9,3 +9,33 @@ const PORT=8080;
 app.listen(PORT, () =>{
     console.log("server started at port " + PORT);
 });
+
+//array of objects
+const names=[
+    {
+        id:1,
+        name:"Ramesh",
+
+    },
+    {
+        id:2,
+        name:"Suresh",
+    },
+];
+
+app.get("/names",(req,res)=>{
+    res.send(names);
+});
+
+app.post("/add",(req,res)=>{
+    const newName= req.body();
+    const dataToAdd=new names(newName);
+    dataToAdd.save();
+    res.send(names);
+});
+
+app.delete("/delete/:id",(req,res)=>{
+    const idToDelete=req.params.id;
+    names.findByIdAndDelete({idToDelete});
+    res.send(names);
+});
